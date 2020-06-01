b160fe6e34a214a45d9bbf95c1717627567c09ab
jamie taffurelli <jltaffurelli@outlook.com>
2020-06-01 15:06:38 +0100

    removed tasks as tehy already exist by microsoft and added tenant deploy logic

--- src/tenant-deploy/tasks/tenant-deploy/v1/tenant-deploy.ps1
+++ src/tenant-deploy/tasks/tenant-deploy/v1/tenant-deploy.ps1
@@ -1,58 +1,26 @@
 . "${PSScriptRoot}\ps_modules\AzureBuilder\cmdlets\interface\Connect-AzureBuilderDevOpsAccount.ps1"
 
-$ResourceGroupName = Get-VstsInput -Name ResourceGroupName -Require
-$ResourceType = Get-VstsInput -Name ResourceName -Default ([String]::Empty)
-$ResourceName = Get-VstsInput -Name ResourceName -Default ([String]::Empty)
-$Action = Get-VstsInput -Name Action -Require
-$LockLevel = Get-VstsInput -Name LockLevel -Default ([String]::Empty)
-$LockName = Get-VstsInput -Name LockName -Default ([String]::Empty)
-$Skip = Get-VstsInput -Name Skip -AsBool
-
 Connect-AzureBuilderDevOpsAccount -Name "ConnectedServiceNameARM"
 
-$params = @{ }
+$location = Get-VstsInput -Name location -Require
+$deploymentName = Get-VstsInput -Name deploymentName -Default (New-Guid)
+$extraParameters = Get-VstsInput -Name deploymentName -Default ([string]::Empty)
 
-if($ResourceName -or ($ResourceName -ne ([String]::Empty)))
+switch(Get-VstsInput -Name templateLocation -Require)
 {
-    $params.Add('ResourceName', $ResourceName)
-    $params.Add('ResourceType', $ResourceType)
-}
-
-switch ($Action)
-{
-    "Lock"
-    { 
-        try 
-        {
-            $lock = Get-AzResourceLock @params -LockName $LockName -ResourceGroupName $ResourceGroupName -ErrorAction Stop
-
-            if(!($lock -and $Skip))
-            {
-                throw "Lock: ${LockName} already exists at scope specified in ${ResourceGroupName}"
-            }
-        }
-        catch
-        {
-            if($_.Exception -like "*LockNotFound*")
-            {
-                New-AzResourceLock -ResourceGroupName $ResourceGroupName @params -LockName $LockName -LockNotes "Az DevOps Pipeline ${LockName}" -LockLevel $LockLevel -Force
-            }
-            else 
-            {
-                throw $_
-            }
-        }
-    }
-    "Unlock"
+    "URL of the file"
     {
-        if($LockName -or ($LockName -ne ([String]::Empty)))
-        {
-            Get-AzResourceLock -ResourceGroupName $ResourceGroupName @params -LockName $LockName -ErrorAction "SilentlyContinue" | Remove-AzResourceLock -Force
-        }
-        else 
-        {
-            Get-AzResourceLock -ResourceGroupName $ResourceGroupName @params | Remove-AzResourceLock -Force
-        }
+        $TemplateUri = Get-VstsInput -Name csmFileLink -Require 
+        $TemplateParameterUri = Get-VstsInput -Name csmParametersFileLink -Require
+
+        Invoke-Expression -Command "New-AzTenantDeployment -Name ${deploymentName} -Location ${location} -TemplateUri ${TemplateUri} -TemplateParameterUri ${TemplateParameterUri} ${extraParameters}"
+    }
+    "Linked artifact"
+    {
+        $TemplateFile = Get-VstsInput -Name csmFile -Require 
+        $TemplateParameterFile = Get-VstsInput -Name csmParametersFile -Require
+
+        Invoke-Expression -Command "New-AzTenantDeployment -Name ${deploymentName} -Location ${location} -TemplateFile ${TemplateFile} -TemplateParameterFile ${TemplateParameterFile} ${extraParameters}"
     }
 }
 
